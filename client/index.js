6133ff7bc0a526dda7aaea9c3210b18f1071aa55
Thomas Lekanger <datagutt@lekanger.no>
2014-06-30 15:08:33 +0200

    Use extended urlencode parsing

--- client/index.js
+++ client/index.js
@@ -33,7 +33,7 @@ function setup(app, siteName){
 	));
 	app.use(express.static(path.join(__dirname, 'public')));
 	app.use(express.static('uploads', nconf.get('api:upload_url')));
-	app.use(bodyParser.urlencoded());
+	app.use(bodyParser.urlencoded({extended: true}));
 	app.use(bodyParser.json());
 	app.use(cookieParser());
 	app.use(expressSession({
